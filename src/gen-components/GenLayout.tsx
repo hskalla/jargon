5609b9b7a5c71b591079aa81c6f9b616d2181929
Henry Skalla <hcskalla@gmail.com>
2024-11-03 17:25:41 +0100

    linked input and output

--- src/gen-components/GenLayout.tsx
+++ src/gen-components/GenLayout.tsx
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './GenLayout.css';
 import Rules from './Rules';
+import {Button} from 'react-native';
 
 function GenLayout() {
+  const [ruleText, setRuleText] = useState('');
+  const [outputText, setOutputText] = useState('initialize');
+  const [gen, setGen] = useState(false);
+
   return (
     <div className='row'>
-        <div className='dynamic-column'>
-            <Rules/>
+        <div className='column'>
+            <Rules ruleText={ruleText} setRuleText={setRuleText}/>
         </div>
-        <div className='static-column'>
-            <p>hello</p>
+        <div className='column'>
+            <div style={{ display: "grid", gridTemplateColumns: "repeat(2, 1fr)", gridGap: 5 }}>
+              <div className='settings-box'>
+                <span>Settings</span>
+                <div className='settings'>
+                  here are the settings
+                </div>
+              </div>
+              <div className='gen-box'>
+                <br/>
+                <Button
+                  onPress={() => setOutputText(ruleText)}
+                  title="Generate"
+                  color="#A846A0"
+                />
+              </div>
+            </div>
+            <div className="output-box">
+            <label form='output'>Output</label>
+              <textarea id='output' cols={1} value={outputText}/>
+            </div>
         </div>
     </div>
   )
--- src/gen-components/Rules.tsx
+++ src/gen-components/Rules.tsx
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import './Rules.css';
+import {Button} from 'react-native';
 
-function Rules() {
+function Rules({ruleText, setRuleText}:any) {
+  const [pressed, setPressed] = useState(false);
+
   return (
     <div className='rules-box'>
         <label form='rules'>Generator Rules</label>
-        <textarea id='rules' cols={1}/>
-        <span>hi</span>
+        <textarea 
+          id='rules' 
+          cols={1} 
+          value={ruleText}
+          onChange={e => (setRuleText(e.target.value))}
+        />
+        <div className='buttons'>
+            <div className='button-box'>
+                <Button
+                  onPress={() => setPressed(true)}
+                  title="Clear"
+                  color="#A846A0"
+                />
+            </div>
+            <div className='button-box'>
+                <Button
+                  onPress={() => setPressed(true)}
+                  title="Save"
+                  color="#A846A0"
+                />
+            </div>
+            <div className='button-box'>
+                <Button
+                  onPress={() => setPressed(true)}
+                  title="Upload"
+                  color="#A846A0"
+                />
+            </div>
+        </div>
     </div>
   )
 }
